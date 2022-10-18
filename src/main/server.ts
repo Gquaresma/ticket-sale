3704cd9195cab91722e5e7b27321861754074228
Wiliane Souza <wiliane_souza@hotmail.com>
2022-10-18 20:30:38 -0300

    add dynamic port

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import Service from "../middleware/src/service";
 
-const PORT = Number(process.env.PORT) || 7071;
+const PORT = Number(process.env.PORT) || 0;
 
 Service.create(PORT, "ticketService")
   .then(async (service) => {
